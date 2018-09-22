abdb37253e473da44f24e36223e52615223d7e5f
League <league@WTS8.attlocal.net>
2018-09-22 16:13:27 -0700

    Moving

---
+++ src/BEEPMessages.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class BEEPMessages {
+	JFrame f = new JFrame("BEEP! Messages");
+	JButton alphanumeric = new JButton("Code");
+	public static void main(String[] args) {
+	BEEPMessages bm = new BEEPMessages();
+	bm.setuphome();
+	}
+	void setuphome() {
+		f
+	}
+
+}
